ddc2b2c2fd655b1976986ad9007299457da45ad3
Gideon <gideonmasters09876@gmail.com>
2025-04-01 10:15:21 -0700

    Interface for factory methods

--- src/mylittlemozart/factory/MidiEventFactory.java
+++ src/mylittlemozart/factory/MidiEventFactory.java
@@ -1,5 +1,10 @@
 package mylittlemozart.factory;
 
-public interface MidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
 
+public interface MidiEventFactory {
+	//Makes methods for Concrete factories to implement
+	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException; 
+	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
