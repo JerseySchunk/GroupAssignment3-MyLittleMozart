28498895b3d6642e6378ab773c909e03e65e1917
Gideon <gideonmasters09876@gmail.com>
2025-04-01 14:33:17 -0700

    Added Legato Factory file and code

---
+++ src/mylittlemozart/factory/LegatoMidiEventFactory.java
@@ -0,0 +1,26 @@
+package mylittlemozart.factory;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
+public class LegatoMidiEventFactory implements MidiEventFactory {
+
+	//variable to decrease tick by 80
+	private static final int duration = 80;
+	//Has MidiEvent for creating a note with noise and no noise
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		ShortMessage on = new ShortMessage();
+		on.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(on, tick);
+	}
+	
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage off = new ShortMessage();
+		off.setMessage(ShortMessage.NOTE_OFF, channel, note);
+		return new MidiEvent(off, tick + duration);
+	}
+	
+}
