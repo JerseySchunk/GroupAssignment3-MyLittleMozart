e654882bbce03669e9e7cbf6d010c3630a193348
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 13:05:47 -0500

    Added imports and list creation

--- src/mylittlemozart/Main.java
+++ src/mylittlemozart/Main.java
@@ -1,9 +1,24 @@
 package mylittlemozart;
 
+import mylittlemozart.csv.MidiEventData;
+import mylittlemozart.csv.MidiCsvParser;
+
+import mylittlemozart.factory.*;
+import mylittlemozart.instrument.*;
+import mylittlemozart.pitch.*;
+
+import javax.sound.midi.*;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			// Reads a CSV file from the path "files/mystery_song.csv"
+			// Parses each line of that file and creates a list of MidiEventData objects from those lines
+			// Stores the objects in a List called midiEvents
+			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("filex/myster_song.csv");
+		}
 
 	}
 
