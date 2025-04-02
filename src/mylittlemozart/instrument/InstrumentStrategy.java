bd7796e52c255facd423317e9d9ad2e993a492d1
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 11:50:31 -0500

    Added javadocs

--- src/mylittlemozart/instrument/InstrumentStrategy.java
+++ src/mylittlemozart/instrument/InstrumentStrategy.java
@@ -3,9 +3,16 @@ package mylittlemozart.instrument;
 import javax.sound.midi.Track;
 
 /**
- * 
+ * InstrumentStrategy interface defines a strategy to assign a MIDI instrument to a channel in a MIDI track.
  */
 
 public interface InstrumentStrategy {
+	
+	/**
+	 * Applies an instrument to a channel in the provided track
+	 * 
+	 * @param track    The MIDI track to apply the instrument to.
+	 * @param channel  The MIDI channel to assign the instrument to.
+	 */
 void applyInstrument(Track track, int channel);
 }
