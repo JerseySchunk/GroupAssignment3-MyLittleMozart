2923fda90b9ee8a5acd2caf7fd631967b05ce945
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 11:19:15 -0500

    Added Javadoc

--- src/mylittlemozart/pitch/PitchStrategy.java
+++ src/mylittlemozart/pitch/PitchStrategy.java
@@ -1,5 +1,8 @@
 package mylittlemozart.pitch;
-
+/**
+ * PitchStrategy is an interface that defines a strategy for modifying the pitch of a MIDI note.
+ * Allows for different ways to alter note pitch. Such as raising or lowering it by a number.
+ */
 public interface PitchStrategy {	
 	int modifPitch(int note);
 }
