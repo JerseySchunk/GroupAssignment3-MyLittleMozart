21e9e18c8d8f2ec3e84f3ec53a0907e34133c24b
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 15:23:18 -0500

    Updated Parser Logic

--- src/mylittlemozart/csv/MidiCsvParser.java
+++ src/mylittlemozart/csv/MidiCsvParser.java
@@ -1,7 +1,9 @@
+package mylittlemozart.csv;
+
 import java.io.*;
 import java.util.*;
 
-import mylittlemozart.csv.MidiEventData;
+import javax.sound.midi.ShortMessage;
 
 public class MidiCsvParser {
     public static List<MidiEventData> parseCsv(String filePath) throws IOException {
@@ -12,13 +14,24 @@ public class MidiCsvParser {
                 String[] values = line.split(",");
                 if (values.length == 6) {
                     try {
-                        int startEndTick = Integer.parseInt(values[0].trim());
-                        int velocity = Integer.parseInt(values[1].trim());
-                        int note = Integer.parseInt(values[2].trim());
-                        int channel = Integer.parseInt(values[3].trim());
-                        int instrument = Integer.parseInt(values[4].trim());
-                        int noteOnOff = Integer.parseInt(values[5].trim());
-
+                    	int startEndTick = Integer.parseInt(values[0].trim());
+                    	String noteOnOffStr = values[1].trim();
+                    	int channel = Integer.parseInt(values[2].trim());
+                    	int note = Integer.parseInt(values[3].trim());
+                    	int velocity = Integer.parseInt(values[4].trim());
+                    	int instrument = Integer.parseInt(values[5].trim());
+                    	
+                    	int noteOnOff;
+                    	
+                    	if(noteOnOffStr.equalsIgnoreCase("Note_on_c")) {
+                    		noteOnOff = ShortMessage.NOTE_ON;
+                    	}else if (noteOnOffStr.equalsIgnoreCase("Note_off_c")) {
+                    		noteOnOff = ShortMessage.NOTE_OFF;
+                    	}else {
+                    		System.out.println("Skipping line with unknown note type: " + line);
+                    		continue;
+                    	}
+                    	
                         events.add(new MidiEventData(startEndTick, velocity, note, channel, instrument, noteOnOff));
                     } catch (NumberFormatException e) {
                         System.out.println("Skipping invalid line: " + line);
