a5bc66d32b92d642189494fa6687054009bb2141
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 12:32:21 -0500

    Implemented strategy to apply trumpet

--- src/mylittlemozart/instrument/TrumpetStrategy.java
+++ src/mylittlemozart/instrument/TrumpetStrategy.java
@@ -1,5 +1,17 @@
 package mylittlemozart.instrument;
 
-public class TrumpetStrategy {
+import javax.sound.midi.*;
 
+public class TrumpetStrategy implements InstrumentStrategy {
+	
+	@Override
+	public void applyInstrument(Track track, int channel) {
+		try {
+			ShortMessage instrumentChange = new ShortMessage();
+			instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 56, 0);
+			track.add(new MidiEvent(instrumentChange, 0));
+		}catch (InvalidMidiDataException e) {
+			e.printStackTrace();
+		}
+	}
 }
