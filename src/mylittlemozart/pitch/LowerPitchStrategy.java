b733937a9b102e865b666499b40d8a9188cc2c70
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 11:21:50 -0500

    Added Javadoc and fixed typos

--- src/mylittlemozart/pitch/LowerPitchStrategy.java
+++ src/mylittlemozart/pitch/LowerPitchStrategy.java
@@ -1,6 +1,11 @@
 package mylittlemozart.pitch;
 
-public class LowerPitchStrategy {
+/**
+ * A pitch strategy that lowers the pitch of a MIDI note by 2 semitones.
+ */
+
+public class LowerPitchStrategy implements PitchStrategy {
+	@Override
 	public int modifyPitch(int note) {
 		return note - 2;
 	}
