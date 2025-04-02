e0f7a1ff121380db069dede5ea7a7f00275db690
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 12:00:00 -0500

    Added javadocs

--- src/mylittlemozart/instrument/ElectricBassGuitarStrategy.java
+++ src/mylittlemozart/instrument/ElectricBassGuitarStrategy.java
@@ -2,11 +2,16 @@ package mylittlemozart.instrument;
 
 import javax.sound.midi.*;
 
+/**
+ * Strategy to apply the ElectricBassGuitar instrument to a given MIDI channel.
+ * Sends a PROGRAM_CHANGE message at the beginning of the track.
+ */
+
 public class ElectricBassGuitarStrategy implements InstrumentStrategy{
 	/**
 	 * Creates a new MIDI message of type 'PROGRAM_CHANGE' which tells the MIDI system to change the instrument for a channel.
 	 * Sets the instrument on the given channel to instrument number 33 which corresponds to ElectricBass
-	 * Wraps the message in a MidiEvent with a timestamp of 0 which means it happens at the start of the track
+	 * Wraps the message in a MidiEvent with a time stamp of 0 which means it happens at the start of the track
 	 * Adds the event to the MIDI Track so that when the MIDI plays, channel 'channel' will sound like a bass
 	 * 
 	 * @param track   The MIDI track to modify.
