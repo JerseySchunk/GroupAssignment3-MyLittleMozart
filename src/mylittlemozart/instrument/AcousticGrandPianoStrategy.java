12c4f4b1fb51f2980c1402f129c81e6172955288
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 12:37:53 -0500

    Implemented AcousiticGrandPiano apply strategy

--- src/mylittlemozart/instrument/AcousticGrandPianoStrategy.java
+++ src/mylittlemozart/instrument/AcousticGrandPianoStrategy.java
@@ -1,5 +1,19 @@
 package mylittlemozart.instrument;
 
-public class AcousticGrandPianoStrategy {
+import javax.sound.midi.*;
+
+public class AcousticGrandPianoStrategy implements InstrumentStrategy {
+	
+	
+	@Override
+	public void applyInstrument(Track track, int channel) {
+		try {
+			ShortMessage instrumentChange = new ShortMessage();
+			instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 0, 0);
+			track.add(new MidiEvent(instrumentChange, 0));
+		}catch (InvalidMidiDataException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
