1f1b0f30bd661af217e294b49772bdd392896191
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 13:22:25 -0500

    Added trumpet and pitch code

--- src/mylittlemozart/Main.java
+++ src/mylittlemozart/Main.java
@@ -17,7 +17,7 @@ public class Main {
 			// Reads a CSV file from the path "files/mystery_song.csv"
 			// Parses each line of that file and creates a list of MidiEventData objects from those lines
 			// Stores the objects in a List called midiEvents
-			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("filex/myster_song.csv");
+			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("filex/mystery_song.csv");
 			
 			
 			//Creates a new MIDI sequence object and creates a new track inside of the sequence
@@ -34,6 +34,14 @@ public class Main {
 			InstrumentStrategy instrumentStrategy = new AcousticGrandPianoStrategy();
 			instrumentStrategy.applyInstrument(track, 0);
 			
+			//reusing instrumentStrategy variable to assign TrumpetStrategy
+			//Applies trumpetStrategy to channel 1 on the track
+			//All notes played on channel 1 will be trumpet
+			instrumentStrategy = new TrumpetStrategy();
+			instrumentStrategy.applyInstrument(track, 1);
+			
+			//Sets up pitch strategy to raise the notes pitch by 2 semitones
+			PitchStrategy pitchStrategy = new HigherPitchStrategy();
 			
 		}
 
