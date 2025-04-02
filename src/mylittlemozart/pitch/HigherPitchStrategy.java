0e628b55fea1941029067dc1a7504cbc392091a2
JerseySchunk <jjsprq@umsystem.edu>
2025-04-02 11:25:51 -0500

    Added Javadocs

--- src/mylittlemozart/pitch/HigherPitchStrategy.java
+++ src/mylittlemozart/pitch/HigherPitchStrategy.java
@@ -1,6 +1,16 @@
 package mylittlemozart.pitch;
 
+/**
+ * A pitch strategy that raises the pitch of a MIDI note by 2 semitones.
+ */
+
 public class HigherPitchStrategy implements PitchStrategy {
+	/**
+	 * Adds 2 to the given MIDI note value to raise pitch
+	 * 
+	 * @param note The original MIDI note value.
+	 * @return The raised MIDI note value.
+	 */
 	@Override
 	public int modifyPitch(int note) {
 		return note + 2;
